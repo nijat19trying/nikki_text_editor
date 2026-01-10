ba747a4e889bbda76c1de95ab91f8e367568261d
nik1917zte <axmedovaska3@gmail.com>
2026-01-10 17:52:37 +0400

    second update

--- nikki.c
+++ nikki.c
@@ -7,7 +7,6 @@
 #include <sys/ioctl.h>
 #include <ctype.h>
 #include <sys/select.h>
-
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define MAX_BUF 1000000
 #define MAX_SEARCH 256
@@ -22,7 +21,7 @@
 #define PAGE_DOWN 1008
 #define BACKSPACE 127
 #define ESC 27
-
+#define MOUSE_CLICK 2000
 struct termios orig_termios;
 char buffer[MAX_BUF];
 int buf_len = 0;
@@ -38,18 +37,16 @@ int search_len = 0;
 int search_mode = 0;
 int match_start = -1;
 int match_end = -1;
-
 void die(const char *msg) {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     perror(msg);
     exit(1);
 }
-
 void disableRawMode() {
+    write(STDOUT_FILENO, "\x1b[?1000l", 9);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
-
 void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
@@ -61,15 +58,14 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 1;
     raw.c_cc[VTIME] = 0;
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    write(STDOUT_FILENO, "\x1b[?1000h", 9);
 }
-
 void getWindowSize() {
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         ws.ws_row = 24;
         ws.ws_col = 80;
     }
 }
-
 void loadFile(const char *name) {
     int fd = open(name, O_RDONLY);
     if (fd == -1) return;
@@ -79,7 +75,6 @@ void loadFile(const char *name) {
     cx = cy = rowoff = coloff = 0;
     modified = 0;
 }
-
 void saveFile() {
     int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1) return;
@@ -87,7 +82,6 @@ void saveFile() {
     close(fd);
     modified = 0;
 }
-
 int editorGetNumRows() {
     int rows = 1;
     for (int i = 0; i < buf_len; i++) {
@@ -95,7 +89,6 @@ int editorGetNumRows() {
     }
     return rows;
 }
-
 int getRowLength(int row) {
     int offset = 0;
     int r = 0;
@@ -106,7 +99,6 @@ int getRowLength(int row) {
     while (offset + len < buf_len && buffer[offset + len] != '\n') len++;
     return len;
 }
-
 int editorCursorToPos(int row, int col) {
     int pos = 0;
     for (int r = 0; r < row; r++) {
@@ -115,7 +107,6 @@ int editorCursorToPos(int row, int col) {
     pos += col;
     return pos;
 }
-
 void bufferPosToCursor(int pos, int *row, int *col) {
     *row = 0;
     *col = 0;
@@ -128,7 +119,6 @@ void bufferPosToCursor(int pos, int *row, int *col) {
         }
     }
 }
-
 void editorMoveCursor(int key) {
     int rowlen = getRowLength(cy);
     int numrows = editorGetNumRows();
@@ -175,13 +165,11 @@ void editorMoveCursor(int key) {
     if (cx < coloff) coloff = cx;
     if (cx >= coloff + ws.ws_col) coloff = cx - ws.ws_col + 1;
 }
-
 void reset_search() {
     search_len = 0;
     search_query[0] = '\0';
     match_start = match_end = -1;
 }
-
 int findNextMatch(int start) {
     if (search_len == 0) return -1;
     for (int i = start; i <= buf_len - search_len; i++) {
@@ -191,24 +179,55 @@ int findNextMatch(int start) {
     }
     return -1;
 }
-
 int editorReadKey() {
     char c;
     if (read(STDIN_FILENO, &c, 1) != 1) die("read");
     if (c == '\x1b') {
-        char seq[3];
-        struct timeval tv = {0, 0};
+        char seq[6];
+        struct timeval tv = {0L, 0L};
         fd_set fdset;
         FD_ZERO(&fdset);
         FD_SET(STDIN_FILENO, &fdset);
         if (select(1, &fdset, NULL, NULL, &tv) != 1) return ESC;
         if (read(STDIN_FILENO, &seq[0], 1) != 1) return ESC;
-        FD_ZERO(&fdset);
-        FD_SET(STDIN_FILENO, &fdset);
-        if (select(1, &fdset, NULL, NULL, &tv) != 1) return ESC;
-        if (read(STDIN_FILENO, &seq[1], 1) != 1) return ESC;
         if (seq[0] == '[') {
-            if (seq[1] >= '0' && seq[1] <= '9') {
+            FD_ZERO(&fdset);
+            FD_SET(STDIN_FILENO, &fdset);
+            if (select(1, &fdset, NULL, NULL, &tv) != 1) return ESC;
+            if (read(STDIN_FILENO, &seq[1], 1) != 1) return ESC;
+            if (seq[1] == 'M') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return ESC;
+                if (read(STDIN_FILENO, &seq[3], 1) != 1) return ESC;
+                if (read(STDIN_FILENO, &seq[4], 1) != 1) return ESC;
+                char button = seq[2] - 32;
+                int mx = seq[3] - 32 - 1;
+                int my = seq[4] - 32 - 1;
+                if (button == 0) {
+                    int target_cy = rowoff + my;
+                    int target_cx = coloff + mx;
+                    int numrows = editorGetNumRows();
+                    if (target_cy >= numrows) target_cy = numrows - 1;
+                    if (target_cy < 0) target_cy = 0;
+                    cy = target_cy;
+                    int rowlen = getRowLength(cy);
+                    if (target_cx > rowlen) target_cx = rowlen;
+                    cx = target_cx;
+                    if (cy < rowoff) rowoff = cy;
+                    if (cy >= rowoff + ws.ws_row - 1) rowoff = cy - (ws.ws_row - 1) + 1;
+                    if (cx < coloff) coloff = cx;
+                    if (cx >= coloff + ws.ws_col) coloff = cx - ws.ws_col + 1;
+                } else if (button == 64) {
+                    rowoff -= 3;
+                    if (rowoff < 0) rowoff = 0;
+                } else if (button == 65) {
+                    int numrows = editorGetNumRows();
+                    int screen_rows = ws.ws_row - 1;
+                    rowoff += 3;
+                    if (rowoff > numrows - screen_rows) rowoff = numrows - screen_rows;
+                    if (rowoff < 0) rowoff = 0;
+                }
+                return MOUSE_CLICK;
+            } else if (seq[1] >= '0' && seq[1] <= '9') {
                 FD_ZERO(&fdset);
                 FD_SET(STDIN_FILENO, &fdset);
                 if (select(1, &fdset, NULL, NULL, &tv) != 1) return ESC;
@@ -235,6 +254,10 @@ int editorReadKey() {
                 }
             }
         } else if (seq[0] == 'O') {
+            FD_ZERO(&fdset);
+            FD_SET(STDIN_FILENO, &fdset);
+            if (select(1, &fdset, NULL, NULL, &tv) != 1) return ESC;
+            if (read(STDIN_FILENO, &seq[1], 1) != 1) return ESC;
             switch (seq[1]) {
                 case 'H': return HOME_KEY;
                 case 'F': return END_KEY;
@@ -245,7 +268,6 @@ int editorReadKey() {
         return c;
     }
 }
-
 void refreshScreen() {
     getWindowSize();
     write(STDOUT_FILENO, "\x1b[?25l", 6);
@@ -297,7 +319,7 @@ void refreshScreen() {
                  "[SEARCH] %s (Esc to exit, Enter = next) ", statusmsg);
     } else {
         snprintf(status, sizeof(status),
-                 "NIKKI  %s%s  |  Ctrl+F Find  |  Ctrl+S Save  |  Ctrl+Q Quit",
+                 "NIKKI %s%s | Ctrl+F Find | Ctrl+S Save | Ctrl+Q Quit",
                  filename, modified ? " [*]" : "");
     }
     write(STDOUT_FILENO, "\x1b[7m", 4);
@@ -313,7 +335,6 @@ void refreshScreen() {
     write(STDOUT_FILENO, buf, strlen(buf));
     write(STDOUT_FILENO, "\x1b[?25h", 6);
 }
-
 void editorDeleteLine() {
     int numrows = editorGetNumRows();
     if (numrows == 0) return;
@@ -333,13 +354,15 @@ void editorDeleteLine() {
     if (cy < rowoff) rowoff = cy;
     if (cy >= rowoff + ws.ws_row - 1) rowoff = cy - (ws.ws_row - 1) + 1;
 }
-
 void editor() {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     enableRawMode();
     getWindowSize();
     while (1) {
         refreshScreen();
         int c = editorReadKey();
+        if (c == MOUSE_CLICK) continue;
         if (search_mode) {
             if (c == ESC || c == CTRL_KEY('f')) {
                 search_mode = 0;
@@ -397,7 +420,7 @@ void editor() {
             case CTRL_KEY('d'):
                 editorDeleteLine();
                 break;
-            case BACKSPACE:
+            case BACKSPACE: {
                 if (cx == 0 && cy == 0) break;
                 int pos = editorCursorToPos(cy, cx);
                 memmove(&buffer[pos - 1], &buffer[pos], buf_len - pos);
@@ -405,22 +428,26 @@ void editor() {
                 editorMoveCursor(ARROW_LEFT);
                 modified = 1;
                 break;
-            case DEL_KEY:
+            }
+            case DEL_KEY: {
                 if (cx == getRowLength(cy) && cy == editorGetNumRows() - 1) break;
-                pos = editorCursorToPos(cy, cx);
+                int pos = editorCursorToPos(cy, cx);
                 memmove(&buffer[pos], &buffer[pos + 1], buf_len - pos - 1);
                 buf_len--;
                 modified = 1;
                 break;
-            case '\r':
-                pos = editorCursorToPos(cy, cx);
+            }
+            case '\r': {
+                int pos = editorCursorToPos(cy, cx);
                 memmove(&buffer[pos + 1], &buffer[pos], buf_len - pos);
                 buffer[pos] = '\n';
                 buf_len++;
                 cy++;
                 cx = 0;
                 modified = 1;
+                if (cy >= rowoff + ws.ws_row - 1) rowoff = cy - (ws.ws_row - 1) + 1;
                 break;
+            }
             case ARROW_LEFT:
             case ARROW_RIGHT:
             case ARROW_UP:
@@ -431,9 +458,9 @@ void editor() {
             case PAGE_DOWN:
                 editorMoveCursor(c);
                 break;
-            default:
+            default: {
                 if (isprint(c) || c == '\t') {
-                    pos = editorCursorToPos(cy, cx);
+                    int pos = editorCursorToPos(cy, cx);
                     memmove(&buffer[pos + 1], &buffer[pos], buf_len - pos);
                     buffer[pos] = c;
                     buf_len++;
@@ -441,10 +468,10 @@ void editor() {
                     modified = 1;
                 }
                 break;
+            }
         }
     }
 }
-
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         fprintf(stderr, "Usage: nikki <filename>\n");
